438bcfcafc36b6078750c5612c15e9d6b9e25267
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-07 16:54:55 +0900

    feat: 클래스형 컴포넌트 ClassCom 생성

---
+++ todolist/src/ClassCom.tsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class ClassCom extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, 컴포넌트!</h1>
+            </div>
+        );
+    }
+}
+
+export default ClassCom;
