01249da6b3ef18d1c357040a3455f24b85daa13f
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-07 16:00:41 +0900

    feat: App 컴포넌트에 조건부 렌더링과 인라인 스타일 추가

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+{
+    /* 
+  작성자 : do
+  작성일 : 2025.04.07
+  내용 : ...
+*/
+}
 
 function App() {
-  return (
-    <div className="container">
-      <h1 className='test'>Hello, 리액트!</h1>
-      <p>반갑습니다.</p>
-    </div>
-  );
+    let name = "리액트";
+
+    return (
+        <div
+            style={{
+                backgroundColor: "grey",
+                color: "white",
+                fontSize: "50px",
+            }}
+        >
+            <h1>Hello, {name === "리액트" ? <h1>YES</h1> : null}!</h1>
+            <p>반갑습니다.</p>
+        </div>
+    );
 }
 
 export default App;
