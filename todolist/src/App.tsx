298e2ce18ab18dff67a77616b164b0af28e14489
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-07 14:34:09 +0900

    feat: App 컴포넌트에 container 및 test 스타일 적용

---
+++ todolist/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className='test'>Hello, 리액트!</h1>
+      <p>반갑습니다.</p>
+    </div>
+  );
+}
+
+export default App;
