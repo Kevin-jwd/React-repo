9ab6dbbc922837a49871c8093eaf660498925626
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-09 18:39:39 +0900

    feat: Todo 항목 id / 체크 여부 추가 및 리스트 map으로 렌더링

--- todolist/src/Todolist.tsx
+++ todolist/src/Todolist.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 
+type Todo = {
+    id: number;
+    text: string;
+    isChecked: boolean;
+};
+
 const TodoList: React.FC = () => {
     const title: string = "오늘 할 일";
 
-    const [todos, setTodos] = useState<string[]>([
-        "잠자기",
-        "공부하기",
-        "코딩하기",
-        "밥먹기",
+    const [todos, setTodos] = useState<Todo[]>([
+        { id: 1, text: "잠자기", isChecked: false },
+        { id: 2, text: "공부하기", isChecked: false },
+        { id: 3, text: "코딩하기", isChecked: false },
+        { id: 4, text: "밥먹기", isChecked: false },
     ]);
 
     return (
@@ -16,10 +22,9 @@ const TodoList: React.FC = () => {
             <div className="container">
                 <div className="board">
                     <ul>
-                        <li>{todos[0]}</li>
-                        <li>{todos[1]}</li>
-                        <li>{todos[2]}</li>
-                        <li>{todos[3]}</li>
+                        {todos.map((todo, index) => (
+                            <li key={index}>{todo.text}</li>
+                        ))}
                     </ul>
                 </div>
             </div>
