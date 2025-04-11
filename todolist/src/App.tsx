1d32d4302038cb37153962c1327204f74972e9cc
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-11 16:39:15 +0900

    feat: App 컴포넌트에 MyWeather 컴포넌트 추가

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -4,6 +4,7 @@ import ClassCom from "./ClassCom";
 import FuncCom from "./FuncCom";
 import TodoList from "./Todolist";
 import Clock from "./Clock";
+import MyWeather from "./MyWeather";
 
 {
     /* 
@@ -19,6 +20,7 @@ function App() {
             <div className="container">
                 <TodoList></TodoList>
                 <Clock></Clock>
+                <MyWeather weather="맑음">일기예보</MyWeather>
             </div>
         </div>
     );
